926bc0e3cd8204aefab3a2c695a2d4065932063c
micheline okito <okitobibi2@gmail.com>
2023-08-24 12:36:17 -0700

    ajout de DOMContent Loaded

--- script.js
+++ script.js
@@ -3,19 +3,7 @@ import{calculate}from "./calculator.js"
 
 // TODO: Faire la manipulation du DOM dans ce fichier
 
-//récuperation de variable
-const affichageHistorique = document.querySelector('#calcul');
-const elementAcalcul = document.querySelector('#input'); 
-const resetElement = document.querySelector('#reset');
-const clearElement = document.querySelector('#clear');
-const operators = document.querySelector('button[type="submit"]');
 
-const chiffres = document.querySelectorAll('.numpad');
 
-//recuperation des elements à afficher dans input
-chiffres.forEach(Number=>(
-   Number.addEventListener('click', (e)=>{
-        elementAcalcul.value += e.target.innerText;
-   })
-))
+
 
